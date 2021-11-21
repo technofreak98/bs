a69e8ef0099b228696558389d5c46d6787dd263f
iamdeepz <deepakchandrasekaran14@gmail.com>
2021-11-21 21:48:51 +0530

    customer form updated

--- src/app/components/customer/customer/customer.component.ts
+++ src/app/components/customer/customer/customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
 
+  customerForm = new FormGroup({
+    customer_name: new FormControl(''),
+    ph_no: new FormControl(''),
+    email: new FormControl(''),
+    address: new FormControl('')
+  })
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onlyNumberKey(evt: any) {
+    // Only ASCII character in that range allowed
+    const ASCIICode = (evt.which) ? evt.which : evt.keyCode;
+    return !(ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57));
+  }
+
+  addCustomer(){
+    console.log(this.customerForm.getRawValue());
   }
 
 }
--- src/app/components/customer/customer/customer.module.ts
+++ src/app/components/customer/customer/customer.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerComponent } from './customer.component';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -10,6 +11,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {path: '', component: CustomerComponent}
     ])
