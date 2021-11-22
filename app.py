1d9a4c246aa91dc3bf6e712a151f5ab2f882e2e5
Naveen <navis9991@gmail.com>
2021-11-22 21:18:25 +0530

    add cors

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS, cross_origin
 import os
 from dotenv import load_dotenv
 
@@ -11,19 +12,22 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 from models import *
 
 
 @app.route('/')
+@cross_origin()
 def hello():
     return "Hello World!"
 
 
 @app.route('/add',methods=['POST'])
+@cross_origin()
 def add_product():
-    product_name = request.form.get('product_name')
-    product_price = request.form.get('product_price')
+    product_name = request.json.get('product_name')
+    product_price = request.json.get('product_price')
     product_metadata = {
         'name' : product_name,
         'price' : product_price
